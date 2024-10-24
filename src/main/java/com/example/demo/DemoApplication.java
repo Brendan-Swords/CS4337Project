18c4308944337549af2edca3c2813942ead5ce52
Brendan Swords <brendanswords21@gmail.com>
2024-10-24 12:52:30 +0100

    made a mistake! fixed now dw

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@PropertySource("file:./secrets.properties")
 public class DemoApplication {
+  @Autowired
+    private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	 @Bean
+    public void printSecrets() {
+        System.out.println("Google Client Secret: " + env.getProperty("GOOGLE_CLIENT_SECRET"));
+    }
 
 }
