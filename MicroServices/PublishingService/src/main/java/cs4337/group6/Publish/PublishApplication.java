fbb01d5f8315a0c638c3b2bfd6f81e12e117cb45
Ominousss <ahmedabdalla7192@gmail.com>
2024-11-12 22:02:03 +0000

    Added exclusion for datasourceautoconfig to allow test to pass

--- MicroServices/PublishingService/src/main/java/cs4337/group6/Publish/PublishApplication.java
+++ MicroServices/PublishingService/src/main/java/cs4337/group6/Publish/PublishApplication.java
@@ -3,7 +3,7 @@ package cs4337.group6.Publish;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PublishApplication {
 
 	public static void main(String[] args) {
