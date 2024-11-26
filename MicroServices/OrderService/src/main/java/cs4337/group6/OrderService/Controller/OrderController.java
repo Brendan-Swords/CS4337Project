f19b88f28584d02f842d7751c5505006da1f6a40
Ominousss <ahmedabdalla7192@gmail.com>
2024-11-26 18:58:16 +0000

    Added GetOrder Get mapping

--- MicroServices/OrderService/src/main/java/cs4337/group6/OrderService/Controller/OrderController.java
+++ MicroServices/OrderService/src/main/java/cs4337/group6/OrderService/Controller/OrderController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class OrderController {
 
@@ -44,6 +46,23 @@ public class OrderController {
             System.err.println("Could not remove Order(ID: " + id + ") due to exception: "  + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(new PostmanResponseMessage<>("Failed to remove Order: " + e.getMessage(), HttpStatus.BAD_REQUEST.value(), null));
+        }
+    }
+
+    @GetMapping(value = "/Orders")
+    public ResponseEntity<PostmanResponseMessage<List<Order>>> GetAllOrders()
+    {
+        try
+        {
+            List<Order> orders = orderService.GetAllOrders();
+            return ResponseEntity.ok(
+                    new PostmanResponseMessage<>("Got a list of orders successfully", HttpStatus.OK.value(), orders));
+        }
+        catch(Exception e)
+        {
+            System.err.println("Could not get the list of Orders due to exception: "  + e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new PostmanResponseMessage<>("Could not get all Orders due to exception: " + e.getMessage(), HttpStatus.BAD_REQUEST.value(), null));
         }
     }
 
